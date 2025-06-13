359df08ff57b779fa7c939144dd3285c379f7448
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-13 16:04:35 +0530

    Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const http = require('http');
+const http = require("http");
+const url = require("url");
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt','utf-8');
@@ -28,10 +29,22 @@ const http = require('http');
 // ______________________________________________________________________________________________
 // SERVER
 const server = http.createServer((req, res) => {
-    // console.log(req);
-    res.end('Hello from the server!');
+  // console.log(req.url);
+
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  }else{
+    res.writeHead(404,{
+        'Content-type':'text/html',
+        'my-own-header' : 'hello-world'
+    });
+    res.end("<h1>PAGE not found!</h1>");
+  }
 });
 
-server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
 });
