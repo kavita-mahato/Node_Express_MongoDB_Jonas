845c9bbaa2612d06b08bf6fc1c9b40cd3293c85d
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-17 04:47:46 +0530

    Responding to URL Parameters

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -30,6 +30,26 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id *1 ;   //to convert to number from string
+
+  // if(id>tours.length){
+  if(!tour){
+    return res.status(404).json({
+      status: 'fail',
+      message : 'Invalid ID'
+    })
+  }
+
+  const tour = tours.find(el => el.id === id)
+  res.status(200).json({
+    status: 'success',
+    data : {
+      tour
+    }
+  });
+});
 
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
