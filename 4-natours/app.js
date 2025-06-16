33e8806d948f1120f3aeb6cb1e4cc2fe15b14840
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-17 03:05:23 +0530

    GET Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,15 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'NATOURS' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'NATOURS' });
+// });
 
-app.post("/", (req, res) => {
-    res.send("You can post to this endpoint...");
+// app.post("/", (req, res) => {
+//     res.send("You can post to this endpoint...");
+// });
+
+//if cons x = JSON.parse
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status:"success",
+    result: tours.length,
+    data: {
+      // then tours : x 
+      tours
+    }
+  });
 });
 
 const port = 3000;
