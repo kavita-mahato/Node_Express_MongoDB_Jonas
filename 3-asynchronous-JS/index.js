640bba95ee6ed03f5bbf82fb3505842bf65c7679
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-16 09:56:05 +0530

    Async Await

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -1,26 +1,43 @@
 const fs = require("fs");
 const { resolve } = require("path");
 const superagent = require("superagent");
-const { reject } = require("superagent/lib/request-base");
+// const { reject } = require("superagent/lib/request-base");
 
 const readFilePro = (file) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, (err, data) => {
-      if (err) reject(`I could not find that file!`);
-      resolve(data);
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) reject(`I could not find that file!`);
+            resolve(data);
+        });
     });
-  });
 };
 
 const writeFilePro = (file, data) => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(file, data, (err) => {
-      if (err) reject("Could not write a file!");
-      resolve("success");
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) reject("Could not write a file!");
+            resolve("success");
+        });
     });
-  });
 };
 
+const getDogPic = async () => {
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`);
+        console.log(`Breed : ${data}`);
+
+        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+        console.log(res.body.message);
+
+        await writeFilePro("dog-img.txt", res.body.message);
+        console.log("Random dog image saved!");
+    } catch (err){
+        console.log(err);
+    }
+};
+getDogPic();
+
+/*
 readFilePro(`${__dirname}/dog.txt`)
   .then((data) => {
     console.log(`Breed : ${data}`);
@@ -43,3 +60,4 @@ readFilePro(`${__dirname}/dog.txt`)
   .catch((err) => {
     console.log(err.message);
   });
+*/
