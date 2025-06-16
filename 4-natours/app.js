e08282068a035266b83efe550c7925104d7bb6fb
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-17 04:01:28 +0530

    POST Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -20,13 +22,34 @@ const tours = JSON.parse(
 
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status:"success",
+    status: 'success',
     result: tours.length,
     data: {
-      // then tours : x 
-      tours
-    }
+      // then tours : x
+      tours,
+    },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
