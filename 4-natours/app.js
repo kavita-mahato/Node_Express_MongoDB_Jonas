b5d3ea75abdb97992fa98143260ded945138e9e9
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-17 07:08:06 +0530

    delete Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -76,14 +76,27 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID'
+      message: 'Invalid ID',
     });
   }
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour here...>'
+      tour: '<Updated tour here...>',
     },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
   });
 });
 
