1b47dd6cddb359ef5649c90717b02fab098f0f9c
Kavita Mahato <kavitamahato0835@gmail.com>
2025-06-17 07:00:35 +0530

    Handling patch requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -32,22 +32,22 @@ app.get('/api/v1/tours', (req, res) => {
 });
 app.get('/api/v1/tours/:id', (req, res) => {
   console.log(req.params);
-  const id = req.params.id *1 ;   //to convert to number from string
+  const id = req.params.id * 1; //to convert to number from string
 
   // if(id>tours.length){
-  if(!tour){
+  if (!tour) {
     return res.status(404).json({
       status: 'fail',
-      message : 'Invalid ID'
-    })
+      message: 'Invalid ID',
+    });
   }
 
-  const tour = tours.find(el => el.id === id)
+  const tour = tours.find((el) => el.id === id);
   res.status(200).json({
     status: 'success',
-    data : {
-      tour
-    }
+    data: {
+      tour,
+    },
   });
 });
 
@@ -70,6 +70,21 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    },
+  });
 });
 
 const port = 3000;
