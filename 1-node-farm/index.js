0fd241d00da1b56cec81788f22160a09924c88ca
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-07 21:08:00 +0530

    Filling the Templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -32,19 +32,56 @@ const url = require("url");
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
+// Reading the files Seperately 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+// Creating a replaceTemplate function to replace the place holders 
+const replaceTemplate = (temp, product) => {
+
+    // Replaces all the place holders with the actual values
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    // If organic then replace otherwise not
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-Organic');
+    }
+    return output;
+}
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
+    // Overview page
     if (pathName === "/" || pathName === "/overview") {
-        res.end("This is the OVERVIEW");
-    } else if (pathName === "/product") {
+        res.writeHead(200, {
+            "Content-type": "text/html",
+        });
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%DESCRIPTION%}', cardsHtml);
+        res.end(output);
+    }
+    // Product page
+    else if (pathName === "/product") {
         res.end("This is the PRODUCT");
-    } else if (pathName === "/api") {
+    }
+    // API 
+    else if (pathName === "/api") {
         res.writeHead(200, {
             "Content-type": "application/json",
         });
         res.end(data);
-    } else {
+    }
+    // Not found 
+    else {
         res.writeHead(404, {
             "Content-type": "text/html",
             "my-own-header": "hello-world",
