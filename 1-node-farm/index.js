9312a1c3b9757352edd4be6585df02b891cff5c1
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-07 16:53:46 +0530

    Building a simple API

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
 //==================================================================
 // FILES
@@ -29,21 +29,29 @@ const url = require('url');
 
 //==================================================================
 // Server
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
-    if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the OVERVIEW');
-    }else if(pathName === '/product'){
-        res.end('This is the PRODUCT');
-    }else{
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
+    if (pathName === "/" || pathName === "/overview") {
+        res.end("This is the OVERVIEW");
+    } else if (pathName === "/product") {
+        res.end("This is the PRODUCT");
+    } else if (pathName === "/api") {
+        res.writeHead(200, {
+            "Content-type": "application/json",
         });
-        res.end('<h1>Page not found!</h1>');
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            "Content-type": "text/html",
+            "my-own-header": "hello-world",
+        });
+        res.end("<h1>Page not found!</h1>");
     }
 });
-server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000...');
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening to requests on port 8000...");
 });
