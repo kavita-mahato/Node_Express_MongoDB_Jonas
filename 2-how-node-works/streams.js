7b8153a27394efd9ddf8039dacb55b5b4793825a
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-08 21:16:58 +0530

    Streams in practice

--- 2-how-node-works/streams.js
+++ 2-how-node-works/streams.js
@@ -5,15 +5,30 @@ const server = require("http").createServer();
 server.on("request", (req, res) => {
     // Solution 1 - Read & Write file
     // In this solution the entire file is read into the memory and then sent to the client
-    fs.readFile("test-file.txt", "utf-8", (err, data) => {
-        if (err) {
-            console.log(err);
-        }
-        res.end(data);
-    });
+    // fs.readFile("test-file.txt", "utf-8", (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //     }
+    //     res.end(data);
+    // });
 
+     // Solution 2 - Streams
+     // In this solution the file is read in chunks and then sent to the client
+     // and does not read the entire file into memory
+     const readable = fs.createReadStream("./test-file.txt"); // creating a readable stream
+
+     // starting the readable stream
+     readable.on("data", (chunk) => {
+          // Start reading "data" in "chunks"
+          res.write(chunk); // then write the chunk to the response
+     });
+
+    // Ending the response when the stream ends
+    readable.on("end", () => {
+        res.end();
+    });
 });
 
 server.listen(8000, "127.0.0.1", () => {
-    console.log("listening to server at port 8000 ...");
+    console.log("listening to server at port 8000...");
 });
