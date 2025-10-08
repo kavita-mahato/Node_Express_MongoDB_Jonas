8e392da690b1de49881a49791c3021ebd35fe3a8
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-08 06:11:54 +0530

    Using 3rd party Modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,10 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+require('slugify');
 
 const replaceTemplate = require('./modules/replaceTemplate');
+const { default: slugify } = require("slugify");
 
 //_______________________________________________________________________________________________
 // FILES
@@ -38,6 +40,10 @@ const dataObj = JSON.parse(data);
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+// slugify to create slugs for each product
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     
