49e002f9adf7ceff7766e1a7dc156e5f052fe1c3
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-09 18:57:20 +0530

    Handling GET Requests

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,16 +1,34 @@
+const fs = require('fs');
 const express = require('express');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    app: 'Natours',
-    message: 'Hello from the Server side!',
-  });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({
+//     app: 'Natours',
+//     message: 'Hello from the Server side!',
+//   });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+//-------------------------------------------------------------------------------------------
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
