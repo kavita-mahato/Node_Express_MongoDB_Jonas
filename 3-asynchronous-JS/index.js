4beb51372383d19ddd5d36f97066a544d82b02a2
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-09 13:40:04 +0530

    Returning Values from Async Function

--- 3-asynchronous-JS/index.js
+++ 3-asynchronous-JS/index.js
@@ -3,40 +3,65 @@ const { get } = require('http');
 const superagent = require('superagent');
 
 const readFilePro = (file) => {
-    return new Promise((resolve, reject) => {
-        fs.readFile(file, (err, data) => {
-            if (err) reject("I couldn't find that file 😢");
-            resolve(data);
-        });
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I couldn't find that file 😢");
+      resolve(data);
     });
+  });
 };
 
 const writeFilePro = (file, data) => {
-    return new Promise((resolve, reject) => {
-        fs.writeFile(file, data, (err) => {
-            if (err) reject('Could not write file 😢');
-            resolve('success');
-        });
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 😢');
+      resolve('success');
     });
+  });
 };
 
 const getDogPic = async () => {
-    try {
-        const data = await readFilePro(`${__dirname}/dog.txt`);
-        console.log(`Breed: ${data}`);
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
 
-        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
 
-        await writeFilePro('dog-img.txt', res.body.message);
-        console.log('Random dog image saved to file!');
-    }
-    catch (err) {
-        console.log(err);
-    }
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('Random dog image saved to file!');
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+  return '2. READY 🐶';
 };
-getDogPic();
 
-// Using Promises
+(async () => {
+  try {
+    console.log('1: Will get dog pics!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  } catch (err) {
+    console.log('ERROR 💥');
+  }
+})();
+
+/*
+console.log('1: Will get dog pics!');
+getDogPic().then((x) => {
+  console.log(x);
+  console.log('3: Done getting dog pics!');
+})
+.catch((err) =>{
+  console.log('ERROR 💥');
+}); */
+
+// Using Promises chaining
 // readFilePro(`${__dirname}/dog.txt`)
 //     .then((data) => {
 //         console.log(`Breed: ${data}`);
