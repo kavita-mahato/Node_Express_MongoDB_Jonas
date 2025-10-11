4dac7be4df22780c276210dde0d042ad699b5b98
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-11 18:22:28 +0530

    Model Testing

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -10,27 +10,42 @@ const DB = process.env.DATABASE;
 
 mongoose.connect(DB).then(() => {
   console.log('DB connection succesful!');
-})
+});
 
 // Tour Schema
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A Tour must have a name'],
-    unique: true
+    unique: true,
   },
   price: {
     type: Number,
-    required: [true, 'A Tour must have a price']
+    required: [true, 'A Tour must have a price'],
   },
   rating: {
     type: Number,
-    default: 4.5
-  }
+    default: 4.5,
+  },
 });
 
 // Tour Model
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 450,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR!💥 : ', err);
+  });
 
 // ----------------------------------------------- STARTING THE SERVER -----------------------------------------------
 const port = process.env.PORT || 3000;
