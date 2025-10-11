ba1db0f5998e3715efc6f1c627cc217b3c83aad2
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-11 14:06:06 +0530

    Creating a simple Tour Model

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -12,6 +12,26 @@ mongoose.connect(DB).then(() => {
   console.log('DB connection succesful!');
 })
 
+// Tour Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A Tour must have a name'],
+    unique: true
+  },
+  price: {
+    type: Number,
+    required: [true, 'A Tour must have a price']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  }
+});
+
+// Tour Model
+const Tour = mongoose.model('Tour', tourSchema);
+
 // ----------------------------------------------- STARTING THE SERVER -----------------------------------------------
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
