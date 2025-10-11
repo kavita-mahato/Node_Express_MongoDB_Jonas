dd9dca24828d024b3ba16cca52f0d86511c2d124
Kavita Mahato <kavitamahato0835@gmail.com>
2025-10-11 13:49:17 +0530

    Connecting DB with Express App

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,9 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 // console.log(process.env.NODE_ENV);
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection succesful!');
+})
 
 // ----------------------------------------------- STARTING THE SERVER -----------------------------------------------
 const port = process.env.PORT || 3000;
